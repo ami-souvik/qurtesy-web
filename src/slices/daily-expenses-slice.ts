a7874d42e2cabdf5dabe693f0c7224581d03df14
Souvik Dey <Ots1589@pidilite.net>
2025-03-24 19:13:10 +0530

    Initial commit

--- src/slices/daily-expenses-slice.ts
+++ src/slices/daily-expenses-slice.ts
@@ -33,91 +33,128 @@ export const fetchTransactions = createAsyncThunk<Transaction[], void, { state: 
   'transactions/list',
   async (_, { getState }) => {
     const state = getState();
+    const section = state.dailyExpenses.section;
     const [year, month] = state.dailyExpenses.yearmonth;
-    return await gtxns('EXPENSE')(year, month);
+    return await gtxns(section)(year, month);
   }
 );
 
 export const createTransaction = createAsyncThunk<Transaction[], CreateTransaction, { state: RootState }>(
   'transactions/create',
-  async (data: CreateTransaction, { dispatch }) => {
-    await ptxn('EXPENSE')(data);
+  async (data: CreateTransaction, { getState, dispatch }) => {
+    const state = getState();
+    const section = state.dailyExpenses.section;
+    await ptxn(section)(data);
     return dispatch(fetchTransactions()).unwrap();
   }
 );
 
 export const updateTransaction = createAsyncThunk<Transaction[], UpdateTransaction, { state: RootState }>(
   'transactions/update',
-  async (data: UpdateTransaction, { dispatch }) => {
+  async (data: UpdateTransaction, { getState, dispatch }) => {
+    const state = getState();
+    const section = state.dailyExpenses.section;
     const { id, ...rest } = data;
-    await puttxn('EXPENSE')(id, rest);
+    await puttxn(section)(id, rest);
     return dispatch(fetchTransactions()).unwrap();
   }
 );
 
 export const deleteTransaction = createAsyncThunk<Transaction[], number, { state: RootState }>(
   'transactions/delete',
-  async (id: number, { dispatch }) => {
-    await dtxn(id);
+  async (id: number, { getState, dispatch }) => {
+    const state = getState();
+    const section = state.dailyExpenses.section;
+    await dtxn(section)(id);
     return dispatch(fetchTransactions()).unwrap();
   }
 );
 
 export const fetchTransactionsSummary = createAsyncThunk<TransactionSummary, void, { state: RootState }>(
   'transactions/summary',
-  gtxnsummary('EXPENSE')
+  async (_, { getState }) => {
+    const state = getState();
+    const section = state.dailyExpenses.section;
+    return gtxnsummary(section)();
+  }
 );
 
-export const fetchCategories = createAsyncThunk<Category[]>('categories/list', gcats('EXPENSE'));
+export const fetchCategories = createAsyncThunk<Category[], void, { state: RootState }>(
+  'categories/list',
+  async (_, { getState }) => {
+    const state = getState();
+    const section = state.dailyExpenses.section;
+    return gcats(section)();
+  }
+);
 
 export const createCategory = createAsyncThunk<Category[], CreateCategory, { state: RootState }>(
   'categories/create',
-  async (data: CreateCategory, { dispatch }) => {
-    await pcat('EXPENSE')(data);
+  async (data: CreateCategory, { getState, dispatch }) => {
+    const state = getState();
+    const section = state.dailyExpenses.section;
+    await pcat(section)(data);
     return dispatch(fetchCategories()).unwrap();
   }
 );
 
 export const updateCategory = createAsyncThunk<Category[], UpdateCategory, { state: RootState }>(
   'categories/update',
-  async (data: UpdateCategory, { dispatch }) => {
+  async (data: UpdateCategory, { getState, dispatch }) => {
+    const state = getState();
+    const section = state.dailyExpenses.section;
     const { id, ...rest } = data;
-    await putcat('EXPENSE')(id, rest);
+    await putcat(section)(id, rest);
     return dispatch(fetchCategories()).unwrap();
   }
 );
 
 export const deleteCategory = createAsyncThunk<Category[], number, { state: RootState }>(
   'categories/delete',
-  async (id: number, { dispatch }) => {
-    await dcat('EXPENSE')(id);
+  async (id: number, { getState, dispatch }) => {
+    const state = getState();
+    const section = state.dailyExpenses.section;
+    await dcat(section)(id);
     return dispatch(fetchCategories()).unwrap();
   }
 );
 
-export const fetchAccounts = createAsyncThunk<Account[]>('accounts/list', gaccs('EXPENSE'));
+export const fetchAccounts = createAsyncThunk<Account[], void, { state: RootState }>(
+  'accounts/list',
+  async (_, { getState }) => {
+    const state = getState();
+    const section = state.dailyExpenses.section;
+    return gaccs(section)();
+  }
+);
 
 export const createAccount = createAsyncThunk<Account[], CreateAccount, { state: RootState }>(
   'accounts/create',
-  async (data: CreateAccount, { dispatch }) => {
-    await pacc('EXPENSE')(data);
+  async (data: CreateAccount, { getState, dispatch }) => {
+    const state = getState();
+    const section = state.dailyExpenses.section;
+    await pacc(section)(data);
     return dispatch(fetchAccounts()).unwrap();
   }
 );
 
 export const updateAccount = createAsyncThunk<Account[], UpdateAccount, { state: RootState }>(
   'accounts/update',
-  async (data: UpdateAccount, { dispatch }) => {
+  async (data: UpdateAccount, { getState, dispatch }) => {
+    const state = getState();
+    const section = state.dailyExpenses.section;
     const { id, ...rest } = data;
-    await putacc('EXPENSE')(id, rest);
+    await putacc(section)(id, rest);
     return dispatch(fetchAccounts()).unwrap();
   }
 );
 
 export const deleteAccount = createAsyncThunk<Account[], number, { state: RootState }>(
   'accounts/delete',
-  async (id: number, { dispatch }) => {
-    await dacc('EXPENSE')(id);
+  async (id: number, { getState, dispatch }) => {
+    const state = getState();
+    const section = state.dailyExpenses.section;
+    await dacc(section)(id);
     return dispatch(fetchAccounts()).unwrap();
   }
 );
